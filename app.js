9f938ac79b107f61371c18894eb607d82f8818c1
Saidamir Botirov <saidamir.botirov@gmail.com>
2022-08-03 19:22:04 +0500

    form element

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-console.log("Hello")
+const PORT = 3000;
+
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+app.use(express.urlencoded({extended:false}));
+
+app.use("/", express.static("pages"))
+
+let inMemoryData = {
+    list: []
+}
+
+app.get("/main", (req, res) => {
+    let filePath = path.join(__dirname, "pages", "main.html")
+    res.sendFile(filePath)
+})
+
+app.get("/contact", (req, res) => {
+    let filePath = path.join(__dirname, "pages", "contact.html")
+    res.sendFile(filePath)
+})
+
+app.get("/about", (req, res) => {
+    let filePath = path.join(__dirname, "pages", "about.html")
+    res.sendFile(filePath)
+})
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/', (req, res) => {
+    res.redirect('/main')
+})
+
+app.get('/list', (req, res) => {
+    res.send(inMemoryData.list)
+})
+
+app.post('/register', (req, res) => {
+    inMemoryData.list.push(req.body)
+    res.redirect('/list')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
